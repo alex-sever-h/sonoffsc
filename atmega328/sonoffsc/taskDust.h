2183ce85eed8bd805761418e1248874d41936765
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2018-03-15 11:45:28 +0200

    Refactor dust readings

--- atmega328/sonoffsc/taskDust.h
+++ atmega328/sonoffsc/taskDust.h
@@ -10,19 +10,22 @@
 #define SHARP_DELTA_TIME     ( 40 - 10)
 #define SHARP_SLEEP_TIME     (9680)
 
+// 0.5V ==> 100ug/m3
+#define VOLT_DUST_RATIO (0.5 / 100)
+#define VOLT_DUST_OFFSET (0.7)
+
 class TaskDust : public TaskPeriodic, public TaskAnalog {
   SerialLink &link_;
 
   int pin_;
   int ledPin_;
-  float dust_mg_m3_;
+  float dust_ug_m3_;
 
   void MYdelayMicroseconds(int us) {
     long start = micros();
     while ((long)micros() - start < us);
   }
 
-  // 0.5V ==> 100ug/m3
   float getDust() {
 
     digitalWrite(ledPin_, LOW);
@@ -33,19 +36,21 @@ class TaskDust : public TaskPeriodic, public TaskAnalog {
     MYdelayMicroseconds(SHARP_DELTA_TIME);
     digitalWrite(ledPin_, HIGH);
 
-    // mg/m3
-    float dust = 170.0 * reading * (5.0 / ADC_COUNTS) - 100.0;
-    if (dust < 0) dust = 0;
-    return dust;
+    float readingVolts = reading * 5.0 / ADC_COUNTS;
+
+    float dust_ug_m3_ = (readingVolts - VOLT_DUST_OFFSET) / VOLT_DUST_RATIO;
+
+    if (dust_ug_m3_ < 0) dust_ug_m3_ = 0;
+    return dust_ug_m3_;
   }
 
   bool work() {
-    dust_mg_m3_ = getDust();
+    dust_ug_m3_ = getDust();
 #if 0
     Serial.print("Dust: ");
-    Serial.println(dust_mg_m3_);
+    Serial.println(dust_ug_m3_);
 #endif
-    if (1) link_.send_P(at_dust, dust_mg_m3_ * 100, false);
+    if (1) link_.send_P(at_dust, dust_ug_m3_ * 100, false);
 
     return true;
   }
